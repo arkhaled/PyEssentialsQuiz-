7aa37623ff575e118f4c950a5731cfdd1735147f
ARARIA KHALED <khaled.araria@univ-sba.dz>
2023-05-10 06:52:44 +0200

    Update LesVariables.py

--- LesVariables.py
+++ LesVariables.py
@@ -1,1 +1,8 @@
+# Exercices
+"""
+Ecrire un programme python qui demande à saisir la longueur et la largeur d'un rectancle, calcule et affiche
+son périmètre et sa surface.
+Ecrire un programme python qui demande à saisir le rayon d'un circle, calcule et affiche son périmètre et sa
+surface.
+"""
 
