8afb6cbff5190dfe8c784a85279b89557ff7c5a3
ARARIA KHALED <khaled.araria@univ-sba.dz>
2023-05-10 07:37:19 +0200

    Update LesVariables.py

--- LesVariables.py
+++ LesVariables.py
@@ -4,14 +4,25 @@
 #  Ecrire un programme python qui demande à saisir le rayon d'un circle, calcule et affiche son périmètre et sa
 #  surface.
 ##################################################################
-
-# ---------- Solution ------------#
+import math 
+# ---------- Solution : Rectancle ------------#
 
 
 longuer_rect=float(input("Entrez la valeur de la longueur du rectangle : A="))   # float()" convertir une chaîne de caractères (str) en un nombre à virgule flottante (float).
 largeur_rect=float(input("Entrez la valeur de la largeur du rectangle : B="))
-surface=longuer_rect*largeur_rect
-Primetre=(longuer_rect+largeur_rect)*2
-print("Surface est S=AxB= {}".format(surface))
-print("Périmètre est P=(A+B)x2= {}".format(Primetre))
+surface_rect=longuer_rect*largeur_rect
+primetre_rect=(longuer_rect+largeur_rect)*2
+print("Surface est S=AxB= {}".format(surface_rect))
+print("Périmètre est P=(A+B)x2= {}".format(primetre_rect))
 
+# ---------- Fin ------------#
+
+# ---------- Solution : Circle------------#
+
+rayon=float(input("Entrez du rayon d'un cercle  : r="))   # float()" convertir une chaîne de caractères (str) en un nombre à virgule flottante (float).
+surface_c=math.pi*rayon**2
+primetre_c=math.pi*rayon*2
+print("Surface est S=3.14 x r²= {}".format(surface_c))
+print("Périmètre est P=3.14 x 2r= {}".format(primetre_c))
+
+# ---------- Fin ------------#
